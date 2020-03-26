3008e8abf8188da9cfb989291f11ca86a4cc523d
MarkoVujnovic <mvujno00@fesb.hr>
2020-03-26 02:56:42 +0100

    First commit

---
+++ Pis_Labosi_Vjezba1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pis_Labosi_Vjezba1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
