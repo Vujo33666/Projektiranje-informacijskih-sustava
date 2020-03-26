278380d23a3080484835712218a6ef9642d844df
MarkoVujnovic <mvujno00@fesb.hr>
2020-03-26 20:19:22 +0100

    Merged2

--- Pis_Labosi_Vjezba1/Class1.cs
+++ Pis_Labosi_Vjezba1/Class1.cs
@@ -6,13 +6,13 @@ namespace Pis_Labosi_Vjezba1
 {
     class Class1
     {
-<<<<<<< HEAD
+
         public void function()
         {
 
         }
-=======
+
         public void funkcijica(){}
->>>>>>> 1fde23109be63ae29136e7e31433998b21c98e22
+
     }
 }
